8da9ac5f6f60973e21e35ba76523511058973af0
Ali <50777787+UNImakeup@users.noreply.github.com>
2020-11-25 20:30:36 +0100

    Nu kan den gemme brugernavn og password, hvis man skriver i felter og trykker på knappen. Så skal vi bare se om det allerede findes før det.

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -8,6 +8,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,17 +32,38 @@ public class MainActivity extends AppCompatActivity {
         final TextView showData = this.findViewById(R.id.show_data); //get the textview.
 
         final ImageView img = this.findViewById(R.id.imageView);
+
+        final EditText username = this.findViewById(R.id.editTextTextPersonName);
+
+        final EditText password = this.findViewById(R.id.editTextTextPersonName3);
+
         img.setImageResource(R.drawable.boat);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance(); //Get instance of database
-        final DatabaseReference myRef = database.getReference("message").child("childmessage"); //Get reference to certain spot in database, tror det er til når jeg prøvede at hente data
+        final DatabaseReference myRef = database.getReference("User"); //Get reference to certain spot in database, tror det er til når jeg prøvede at hente data. Også når jeg indsætter data.
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //String a= dataSnapshot.getValue().toString();
+                if(dataSnapshot.child("4").exists()) { //Kan se om en bestemt bruger eksisterer. Kan bruges når vi skal oprette ny bruger. Hvis den ikke eksisterer, tilføjer vi data, ved at sige setValue, ved et bestemt child.
+                    showData.setText("has aids");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         playsound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) { //When button playsound is clicked.
                 lyd.start(); //Play sound
 
-                myRef.setValue("Hello, World!"); //Send data to database
+                myRef.child(username.getText().toString()).child("password").setValue(password.getText().toString()); //Send data to database
                 
 
                 myRef.addValueEventListener(new ValueEventListener() {
@@ -50,8 +72,13 @@ public class MainActivity extends AppCompatActivity {
                         if(dataSnapshot.exists()){
                             //String greeting = dataSnapshot.child("message").child("childmessage").getValue().toString();
                             String greeting = dataSnapshot.getValue().toString();
-                            showData.setText(greeting);
+                            if(!dataSnapshot.getValue().toString().equals(null)){
+                                System.out.println("wagwan blud, no user");
+                                showData.setText("no User");
+                            }
+                            //showData.setText(greeting);
                             img.setImageResource(R.drawable.lol);
+                            //img.setVisibility(View.INVISIBLE);
                         }
 
                     }

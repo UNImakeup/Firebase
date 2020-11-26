b5e3307293a9d30633a3e7b56ee9e4686bfbab4e
Ali <50777787+UNImakeup@users.noreply.github.com>
2020-11-26 17:45:53 +0100

    Den kan tjekke for password nu, men hvis man skriver forkert brugernavn crasher programmet.

--- app/src/main/java/com/example/firebase/MainActivity2.java
+++ app/src/main/java/com/example/firebase/MainActivity2.java
@@ -1,5 +1,6 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,6 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
@@ -15,19 +22,46 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        EditText userName = findViewById(R.id.editTextTextPersonName2);
+        final EditText userName = findViewById(R.id.editTextTextPersonName2);
 
-        EditText password = findViewById(R.id.editTextTextPersonName4);
+        final EditText password = findViewById(R.id.editTextTextPersonName4);
         Button login = findViewById(R.id.button);
 
         final TextView display = findViewById(R.id.textView);
+
+        final FirebaseDatabase[] database = {FirebaseDatabase.getInstance()}; //Get instance of database
+        final DatabaseReference myRef = database[0].getReference("User"); //Get reference to certain spot in database, tror det er til når jeg prøvede at hente data. Også når jeg indsætter data.
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Tjek om brugernavn og kode stemmer overens.
                 //Bare se om den med child der er brugernavn har child med det password. Så bare sige username eller password forkert og ellers sig, velkommen + username
-                display.setText("you have attempted a login.");
+
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            //display.setText(dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString());
+
+                            if(dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString().equals(password.getText().toString())){
+                                display.setText("You have inputtet a matching pair of username and password! :)");
+                            } else{
+                                display.setText("wrong username or password. :(");
+                            }
+
+
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                //display.setText("you have attempted a login.");
 
             }
         });

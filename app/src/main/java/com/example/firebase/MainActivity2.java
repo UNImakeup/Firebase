55c4a84a63bc5389c08d98f0a27a9c0a90ca1d9e
Ali <50777787+UNImakeup@users.noreply.github.com>
2020-12-01 13:12:33 +0100

    Har tilføjet en kommentar, måske andet, halla

--- app/src/main/java/com/example/firebase/MainActivity2.java
+++ app/src/main/java/com/example/firebase/MainActivity2.java
@@ -40,7 +40,7 @@ public class MainActivity2 extends AppCompatActivity {
         final SharedPreferences gemmeobjekt = PreferenceManager.getDefaultSharedPreferences(this);
         final String user = gemmeobjekt.getString("username", "");
 
-        if(!user.isEmpty()){
+        if(!user.isEmpty()){ //Hvis der er en bruger logget ind. Burde nok gøre det på den første side. Så kan man enten lave en bruger eller logge ind, hvis man ikke er det.
             Intent homeIntent = new Intent(MainActivity2.this, Home.class);
             startActivity(homeIntent);
         }
@@ -53,34 +53,37 @@ public class MainActivity2 extends AppCompatActivity {
                 //Tjek om brugernavn og kode stemmer overens.
                 //Bare se om den med child der er brugernavn har child med det password. Så bare sige username eller password forkert og ellers sig, velkommen + username
 
-                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //database[0].
 
-                            //display.setText(dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString());
-                            if(dataSnapshot.child(userName.getText().toString()).exists()) {
-                                if (dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString().equals(password.getText().toString())) {
-                                    gemmeobjekt.edit().putString("username", userName.getText().toString()).commit();
-                                    User thisUser = new User(gemmeobjekt.getString("username", "")); //Prøver at lave et brugerobjekt med brugernavnet, men tror singleton giver mening her, da vi skal bruge den samme bruger, men hellere vil have den fra rammen end harddisk.
-                                    display.setText("You have inputtet a matching pair of username and password! :) welcome " + gemmeobjekt.getString("username", "User"));
-                                    HomeButton.setVisibility(View.VISIBLE);
 
-                                } else {
-                                    display.setText("wrong password. :(");
-                                }
+                        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        //display.setText(dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString());
+                        if(dataSnapshot.child(userName.getText().toString()).exists()) {
+                            if (dataSnapshot.child(userName.getText().toString()).child("password").getValue().toString().equals(password.getText().toString())) {
+                                gemmeobjekt.edit().putString("username", userName.getText().toString()).commit();
+                                User thisUser = new User(gemmeobjekt.getString("username", "")); //Prøver at lave et brugerobjekt med brugernavnet, men tror singleton giver mening her, da vi skal bruge den samme bruger, men hellere vil have den fra rammen end harddisk.
+                                display.setText("You have inputtet a matching pair of username and password! :) welcome " + gemmeobjekt.getString("username", "User"));
+                                HomeButton.setVisibility(View.VISIBLE);
+
                             } else {
-                                display.setText("Wrong username and/or password");
+                                display.setText("wrong password. :(");
                             }
-
-
-
+                        } else {
+                            display.setText("Wrong username and/or password");
                         }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                        }
-                    });
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
                 //display.setText("you have attempted a login.");
 
